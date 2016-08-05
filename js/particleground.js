329c6d022b84d487d28761d0cad59f8e09476964
Manuel Dionne <mandio0302@gmail.com>
2016-08-05 20:06:16 -0300

    Updated visuals
    made the colors and font more pleasing

--- js/particleground.js
+++ js/particleground.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#222222',
-    lineColor: '#111111'
+    dotColor: '#1c2f5a',
+    lineColor: '#29468a'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
