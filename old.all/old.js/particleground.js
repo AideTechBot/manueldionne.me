3b6d172532b48b7fd1bb158bd9ac947621e4dd29
AideTechBot <mandio0302@gmail.com>
2017-01-12 12:44:56 -0500

    Cleared everything to start over

--- old.all/old.js/particleground.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  particleground(document.getElementById('particles'), {
-    dotColor: '#1c2f5a',
-    lineColor: '#29468a'
-  });
-  var intro = document.getElementById('intro');
-  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
-}, false);
